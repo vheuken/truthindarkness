10e9a0dd5dbaa999166963b67e6ff6283957bdd3
vheuken <vheuken@f1d08a3e-75eb-b442-7451-a4f1cee21b5b>
2010-08-11 02:28:31 +0000

    added Map.h(incomplete)

---
+++ trunk/src/Map.h
@@ -0,0 +1,33 @@
+/*
+This file is part of "Truth in Darknesss".
+
+"Truth in Darkness" is free software: you can redistribute it 
+and/or modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation, either version 3 of the 
+License, or (at your option) any later version.
+
+"Truth in Darkness" is distributed in the hope that 
+it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See 
+the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with "Truth in Darkness". If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef MAP_H
+#define MAP_H
+
+#include "SpriteSheet.h"
+
+class Map
+{
+private:
+	// obtains data from the map file
+	void mapFileparser();
+
+public:
+
+};
+
+#endif // MAP_H
